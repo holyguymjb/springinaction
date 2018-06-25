313472a4eb2c9dbe561bbe80f356e25a198f02b5
minjb <minjb@keruyun.com>
2018-06-25 14:09:43 +0800

    删除idea文件

--- ch05/src/main/java/spittr/config/RootConfig.java
+++ ch05/src/main/java/spittr/config/RootConfig.java
@@ -9,12 +9,13 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.type.filter.RegexPatternTypeFilter;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import spittr.config.RootConfig.WebPackage;
 
 @Configuration
 @ComponentScan(basePackages={"spittr"},
     excludeFilters={
-        @Filter(type=FilterType.CUSTOM, value=WebPackage.class)
+        @Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)
     })
 public class RootConfig {
   public static class WebPackage extends RegexPatternTypeFilter {
---
+++ ch05/src/main/java/spittr/web/WebConfig.java
@@ -0,0 +1,29 @@
+package spittr.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spittr.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setExposeContextBeansAsAttributes(true);
+        return viewResolver;
+    }
+}
